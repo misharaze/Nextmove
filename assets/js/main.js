3fbd7f3253428ef33d5e68cfde31c26f22725cc0
mikhail guilbo <mikhailguilbo@MacBook-Pro-mikhail.local>
2025-12-11 00:34:12 +0100

    change something

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,25 @@ function showToast(message) {
     });
   });
 }
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  if (loader) {
+    loader.classList.add("hidden");
+  }
+});
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
